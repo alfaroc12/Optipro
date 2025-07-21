088f2e5c8d339e5dd813cd64197a249df1481e12
alfaroc12 <karlozsupra@gmail.com>
2025-07-20 20:41:07 -0500

    Correcciones en el Statict root

--- Backend/optipro/asgi.py
+++ Backend/optipro/asgi.py
@@ -1,25 +1,17 @@
 """
-ASGI config for optipro project.
+ASGI config for Gestor_Citas project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import notifications.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'optipro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gestor_Citas.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            notifications.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
+
